d09291f8cf7ad18e8798d477eea4feb087c1b9be
zang <1211987254@qq.com>
2026-04-01 22:23:45 +0800

    Template Pattern

---
+++ TemplatePattern-14.cpp
@@ -0,0 +1,14 @@
+#include "TemplatePattern-14.h"
+
+using namespace std;
+
+int main()
+{
+    ComputerGame game1;
+    FriendGame game2;
+
+    game1.start();
+    std::cout<<std::endl;
+    game2.start();
+    return 0;
+}
